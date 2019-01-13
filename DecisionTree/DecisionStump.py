9444fc6e8b1b8cb5924f211fbabffb475e3e2758
PeteFa <18991376110@163.com>
2019-01-13 22:10:24 +0800

    decision stump

---
+++ DecisionTree/DecisionStump.py
@@ -0,0 +1,363 @@
+import numpy as np
+import cvxopt as cvx
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import axes3d
+import math
+
+#from homework5.homework5 import LinearSVM
+
+global YLoc
+YLoc = 2
+
+global weightLoc
+weightLoc = 3
+
+global featureNum
+featureNum = 2
+
+def DoAdaptiveBoost():
+
+    class AdaptiveBoost():
+        def __init__(self):
+            #self._b = np.zeros(1,dtype=np.float64)
+            self._w = np.zeros(2)
+            self.YLoc = 2    # Y值的位置
+            self._trainLen = 10
+            self._trainNum = 20
+
+        def __sign__(self,a):
+            if a >= 0.0:
+                return 1
+            elif a < 0.0:
+                return -1
+
+        def __generateTrainData__(self):
+            X1 = np.random.uniform(-1.0,1.0,self._trainLen)
+            X2 = np.random.uniform(-1.0,1.0,self._trainLen)
+            randomY  = np.random.uniform(-1.0,1.0,self._trainLen)
+            Y = []
+            for loop in range(self._trainLen):
+                Y.append(self.__sign__(randomY[loop]))
+
+            Y = np.array(Y)
+
+            X1 = np.array([-0.24477302,0.19475279,0.25548862,-0.54018758,0.30140678,-0.34477604,0.13941788,0.16259198,0.26696617,-0.75998417])
+            X2 = np.array([ 0.51412259,0.28742792,0.4249421, 0.36054774,-0.60541032,0.3538119,-0.043742,0.52602336,0.76907062,0.99818954])
+            Y  = np.array([-1,-1,-1,1,1,-1,1,-1,1,1])
+            self.X1 = X1
+            self.X2 = X2
+            self.Y  = Y
+            self.DataSet = np.column_stack((X1, X2, Y))
+            self.trainSet = self.DataSet
+
+        def __genTrainSetWithBootstrapping__(self,seed):
+            randomArray = np.around(np.random.uniform(0, self._trainLen - 1, self._trainLen), decimals=0)
+
+            trainSet = np.zeros((1, 3))
+
+            '''
+            randomArray = np.zeros(1)
+            for loop in range(self._trainLen):
+                randomArray = np.append(randomArray,loop)
+
+            randomArray = np.delete(randomArray,0,0)
+            
+            for loop in range(randomArray.size):
+                if seed == randomArray.size - 1:
+                    randomArray[randomArray.size - 1] = randomArray[0]
+                else:
+                    randomArray[seed] = randomArray[seed + 1]
+            '''
+
+            for loop in range(randomArray.size):
+                arrayIndex = int(randomArray[loop])
+                element = self.DataSet[arrayIndex]
+                element = np.reshape(element,(1,3))
+                trainSet = np.concatenate((trainSet, element), axis=0)
+
+            trainSet = np.delete(trainSet, 0, 0)
+            self.trainSet = trainSet[np.lexsort(trainSet[:, ::-1].T)]  # 令人窒息的按第一列排序的操作
+
+        def __genTrainInterVal__(self,featureIdx):
+            intervalList = []
+            self.trainSet = self.trainSet[np.lexsort(self.trainSet[:, ::-1].T)]  # 令人窒息的按第一列排序的操作
+            for loop in range(self.trainSet.shape[0]):
+                if ((loop + 1) == self.trainSet.shape[0]):
+                    break
+                intervalList.append((self.trainSet[loop][featureIdx] + self.trainSet[loop + 1][featureIdx]) / 2)
+
+            return np.array(intervalList)
+
+        def __SeekMinError__(self,trainProcSet):
+            sList = [1, -1]
+            global YLoc
+            global weightLoc
+            global featureNum
+
+            minError = self._trainLen
+            minS = 0
+            minTheta = 0
+            minSigmalUnOfError = 0
+            minSigmaUn = 0
+            minfeatureIdx = 0
+            minRealError  = 0
+            for featureIdx in range(featureNum):
+                midInterval = self.__genTrainInterVal__(featureIdx)  # 生成待分割点中的中值，用于分割线定位 Theta self.midInterval
+                for s in sList:
+                    for theta in midInterval:
+                        error = 0
+                        sigmalUn = 0
+                        sigmalUnOfError = 0
+                        realError = 0
+                        for loop in range(trainProcSet.shape[0]):
+                            sigmalUn += trainProcSet[loop][weightLoc]
+                            val = trainProcSet[loop][featureIdx] - theta
+                            if (trainProcSet[loop][YLoc] != s * self.__sign__(val)):
+                                error += trainProcSet[loop][weightLoc]
+                                realError += 1
+                                sigmalUnOfError += trainProcSet[loop][weightLoc]
+
+                        if error < minError:
+                            minError = error
+                            minS = s
+                            minTheta = theta
+                            minSigmaUn = sigmalUn
+                            minSigmalUnOfError = sigmalUnOfError
+                            minRealError = realError
+                            minfeatureIdx = featureIdx
+
+            elpsilonT = minSigmalUnOfError / minSigmaUn
+            if (minError != 0):
+                DiamondT = math.sqrt((1 - elpsilonT) / elpsilonT)
+            else:
+                DiamondT = 999999
+
+            for loop in range(trainProcSet.shape[0]):
+                if (trainProcSet[loop][YLoc] != minS * self.__sign__(trainProcSet[loop][minfeatureIdx] - minTheta)):
+                    print('weight1:', trainProcSet[loop][weightLoc])
+                    trainProcSet[loop][weightLoc] *= DiamondT
+                    print ('DiamondT:',DiamondT)
+                    print ('weight2:',trainProcSet[loop][weightLoc],',*:',trainProcSet[loop][weightLoc] * DiamondT)
+                else:
+                    trainProcSet[loop][weightLoc] /= DiamondT
+
+
+            alpha = math.log(DiamondT)
+            #print('alpha:',alpha,'minRealError:',minRealError,'minS:',minS,'minTheta:',minTheta,'featureIdx:',minfeatureIdx)
+
+            #print ('***** trainProcSecond ***********')
+            print (trainProcSet)
+            return (alpha,minRealError,minS,minTheta,minfeatureIdx)
+
+
+        def __sketchLine__(self,fig,ax,mapInfo,trainProcSet):
+            for i in range(mapInfo.shape[0]):
+                thetaList = mapInfo[i][0] * np.ones(2)
+                Yaxix = np.array([-1, 1])
+                if 0 == mapInfo[i][1]:
+                    ax.plot(thetaList, Yaxix)
+                else:
+                    ax.plot(Yaxix, thetaList)
+
+
+        def __checkTrainResult__(self,funcMatrix,index,trainProcSet):
+            '''
+            error = 0
+            for loopi in range(self.DataSet.shape[0]):
+                G = 0
+                for loopk in range(funcMatrix.shape[0]):
+                    (alpha,s,theta) = funcMatrix[loopk][0],funcMatrix[loopk][2],funcMatrix[loopk][3]
+                    if (0 == funcMatrix[loopk][4]):
+                        g = alpha * s * (self.DataSet[loopi][0] - theta)
+                    else:
+                        g = alpha * s * (self.DataSet[loopi][1] - theta)
+                    G += g
+                if (self.DataSet[loopi][YLoc] != self.__sign__(G)):
+                    print (self.DataSet[loopi][YLoc],G)
+                    error += 1
+            '''
+            error = 0
+            G = 0
+            global featureNum
+
+            print('**************    funcMatrix     ***************')
+            print('alpha    errorRate    minS    minTheta    featureIdx')
+            print ('funcMatrix:',funcMatrix)
+            for dataLoop in range(self.DataSet.shape[0]):
+                for funLoop in range(funcMatrix.shape[0]):
+                    (alpha, s, theta) = funcMatrix[funLoop][0], funcMatrix[funLoop][2], funcMatrix[funLoop][3]
+                    if (0 == funcMatrix[funLoop][4]):
+                        g = alpha * s * (self.DataSet[dataLoop][0] - theta)
+                    else:
+                        g = alpha * s * (self.DataSet[dataLoop][1] - theta)
+                    G += g
+                G /= funcMatrix.shape[0]
+                if (self.DataSet[dataLoop][YLoc] != self.__sign__(G)):
+                    print (self.DataSet[dataLoop][YLoc],G)
+                    error += 1
+            print ('Train Count:',index,',accurate:',1 - error/self.DataSet.shape[0],'error:',error)
+            pass
+
+
+        def __train__(self):
+            # h(x) = s * sign(x - theta)
+            sList = [1,-1]
+
+            #print ('X1',self.X1)
+            #featureX =  np.concatenate((self.X1,self.X2))
+            #featureX = np.reshape(featureX,(2,self.X1.size))
+            #print('FeatureX: ',featureX)
+
+            #fig, ax = plt.subplots(1, 1)
+            #fig, ax = self.__sketchFrame__(fig,ax)
+
+            funcMatrix = np.zeros((1,5))
+
+            mapInfo = np.zeros((1,2))
+            count   = 0
+            loopCount = 0
+            weight = np.ones(self.trainSet.shape[0]) / self.trainSet.shape[0]
+            trainProcSet = np.column_stack((self.trainSet, weight))
+
+            for loop in range(self._trainNum):
+                loopCount += 1
+                #self.__genTrainSetWithBootstrapping__(loop)   # 利用Bootstrapping生成训练集 self.trainSet
+
+                print (trainProcSet)
+                (alpha,errorRate,minS,minTheta,featureIdx)= self.__SeekMinError__(trainProcSet)  # 寻找 self.trainSet 中最小Error对应的s，theta
+                element = np.array([(alpha,errorRate,minS,minTheta,featureIdx)])
+                element = np.reshape(element,(1,5))
+                funcMatrix = np.concatenate((funcMatrix,element),axis=0)
+                if 1 == loopCount:
+                    funcMatrix = np.delete(funcMatrix, 0, 0)
+
+                '''
+                thetaList = minTheta * np.ones(2)
+                Yaxix = np.array([-1, 1])
+                if 0 ==  featureIdx:
+                    ax.plot(thetaList, Yaxix)
+                else:
+                    ax.plot(Yaxix,thetaList )
+                '''
+                element = np.reshape(np.array([minTheta,featureIdx]),(1,2))
+                mapInfo = np.concatenate((mapInfo,element),axis=0)
+                fig, ax = plt.subplots(1, 1)
+                fig, ax = self.__sketchFrame__(fig, ax, trainProcSet)
+                count += 1
+                if count == 1:
+                    mapInfo = np.delete(mapInfo,0,0)
+
+
+                self.__sketchLine__(fig, ax,mapInfo,trainProcSet)
+                self.__checkTrainResult__(funcMatrix,loopCount,trainProcSet)
+                #print('**************    funcMatrix     ***************')
+                #print('alpha    errorRate    minS    minTheta    featureIdx')
+                #print(funcMatrix)
+                plt.show()
+                pass
+
+            print ('**************    funcMatrix     ***************')
+            print ('alpha    errorRate    minS    minTheta    featureIdx')
+            print (funcMatrix)
+            #self.__checkTrainResult__(funcMatrix,100)
+
+            plt.show()
+
+
+        def __sketchFrame__(self,fig,ax, trainProcSet):
+            global weightLoc
+            correctListX1 = []
+            correctListX2 = []
+
+            errorListX1 = []
+            errorListX2 = []
+
+            correctY = []
+            errorY = []
+
+            print (self.X1)
+            for loop in range(self.Y.shape[0]):
+                weight = 0
+                for pointLoop in range(trainProcSet.shape[0]):
+                    if (self.X1[loop] == trainProcSet[pointLoop][0]) and (self.X2[loop] == trainProcSet[pointLoop][1]):
+                        weight = trainProcSet[pointLoop][weightLoc]
+
+                size = 1000 * weight
+                print (loop,size)
+                if self.Y[loop] == 1:
+                    correctListX1.append(self.X1[loop])
+                    correctListX2.append(self.X2[loop])
+                    correctY.append(size)
+                else:
+                    errorListX1.append(self.X1[loop])
+                    errorListX2.append(self.X2[loop])
+                    errorY.append(size)
+
+            correctArrayX1 = np.array(correctListX1)
+            correctArrayX2 = np.array(correctListX2)
+            errorArrayX1 = np.array(errorListX1)
+            errorArrayX2 = np.array(errorListX2)
+            correctY = np.array(correctY)
+            errorY = np.array(errorY)
+
+            ax.scatter(correctArrayX1, correctArrayX2, correctY, c='g', marker="o")
+            ax.scatter(errorArrayX1, errorArrayX2, errorY, c='b', marker="x")
+
+            plt.xlabel("X1")
+            plt.ylabel("X2")
+            fig.suptitle('Adaptive Boost ')
+
+            return fig,ax
+
+        def __sketchTrainDataMap__(self,X1,X2,Y,s,theta):
+            correctListX1 = []
+            correctListX2 = []
+
+            errorListX1 = []
+            errorListX2 = []
+
+            correctY = []
+            errorY   = []
+
+            for loop in range(Y.shape[0]):
+                if Y[loop] == 1:
+                    correctListX1.append(X1[loop])
+                    correctListX2.append(X2[loop])
+                    correctY.append(100)
+                else:
+                    errorListX1.append(X1[loop])
+                    errorListX2.append(X2[loop])
+                    errorY.append(100)
+
+            correctArrayX1 = np.array(correctListX1)
+            correctArrayX2 = np.array(correctListX2)
+            errorArrayX1   = np.array(errorListX1)
+            errorArrayX2    = np.array(errorListX2)
+            correctY       = np.array(correctY)
+            errorY         = np.array(errorY)
+
+            fig, ax = plt.subplots(1, 1)
+            ax.scatter(correctArrayX1, correctArrayX2,correctY,c='g', marker="o")
+            ax.scatter(errorArrayX1, errorArrayX2,errorY,c='b', marker="x")
+
+            thetaList = theta * np.ones(2)
+            Yaxix = np.array([-1,1])
+            ax.plot(thetaList,Yaxix)
+            print(thetaList)
+            print(X1)
+
+
+            plt.xlabel("X1")
+            plt.ylabel("X2")
+            fig.suptitle('Adaptive Boost ')
+
+            plt.show()
+
+    softMarginSVMObj = AdaptiveBoost()
+    softMarginSVMObj.__generateTrainData__()
+    softMarginSVMObj.__train__()
+
+
+if __name__ == "__main__":
+    DoAdaptiveBoost()
--- SVM/SoftMarginSVM.py
+++ SVM/SoftMarginSVM.py
@@ -4,203 +4,9 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import axes3d
 
+def DoSoftMarginSVM():
 
-def SolveQPWithCvxopt():
-    P = cvx.matrix([[1.0, 0.0], [0.0, 0.0]])  # matrix里区分int和double，所以数字后面都需要加小数点
-    q = cvx.matrix([3.0, 4.0])
-    G = cvx.matrix([[-1.0, 0.0, -1.0, 2.0, 3.0], [0.0, -1.0, -3.0, 5.0, 4.0]])
-    h = cvx.matrix([0.0, 0.0, -15.0, 100.0, 80.0])
-
-    sol = cvx.solvers.qp(P, q, G, h)  # 调用优化函数solvers.qp求解
-    print(sol['x'])  # sol['x'] 里面是对应的最优解
-
-    P = cvx.matrix(np.diag([1.0, 0]))  # 对于一些特殊矩阵，用numpy创建会方便很多（在本例中可能感受不大）
-    q = cvx.matrix(np.array([3.0, 4]))
-    G = cvx.matrix(np.array([[-1.0, 0], [0, -1], [-1, -3], [2, 5], [3, 4]]))
-    h = cvx.matrix(np.array([0.0, 0, -15, 100, 80]))
-    sol = cvx.solvers.qp(P, q, G, h)
-    print(sol['x'])  # 打印结果，sol里面还有很多其他属性，读者可以自行了解
-
-def DoLinearSVM():
-
-    class LinearSVM():
-        def __init__(self):
-            #self._b = np.zeros(1,dtype=np.float64)
-            self._w = np.zeros(2)
-
-        def __sign__(self,a):
-            if a >= 0.0:
-                return 1
-            elif a < 0.0:
-                return -1
-
-        def __generateTrainData__(self):
-            X1 = np.random.uniform(-1.0,1.0,200)
-            X2 = np.random.uniform(-1.0,1.0,200)
-            B  = 0.03
-            W1=  0.3
-            W2=  0.8
-            Y = np.zeros(200)
-            for loop in range(Y.shape[0]):
-                Y[loop] = self.__sign__(W1 * X1[loop] + W2 * X2[loop] + B)
-
-            '''
-            noiseArray = np.random.rand(Y.shape[0])
-            for loop in range(Y.shape[0]):
-                if (noiseArray[loop]) > 0.9:
-                    Y[loop] = -1 * Y[loop]
-            '''
-
-            Xarray = np.stack((X1, X2), axis=-1)
-
-            self._X  = Xarray
-            self._Y = Y
-            self.sketchTrainDataMap()
-
-
-
-
-        def train(self):
-            Q = cvx.matrix(np.diag([0.0, 1.0,1.0])) # Q =[[0,0,0],[0,1,0],[0,0,1]]
-
-            for loop in range(self._Y.shape[0]):
-                if loop == 0:
-                    A = np.array([[-1.0*self._Y[loop],
-                                 np.multiply(self._Y[loop] *-1.0,self._X[loop][0]),
-                                 np.multiply(self._Y[loop] *-1.0,self._X[loop][1])]])
-                else:
-                    rowArray = np.array([[-1.0*self._Y[loop],
-                                 np.multiply(self._Y[loop] * -1.0,self._X[loop][0]),
-                                 np.multiply(self._Y[loop] * -1.0,self._X[loop][1])]])
-                    A = np.concatenate((A, rowArray), axis=0)
-
-            cn_array = np.multiply(-1,np.ones((200,)))
-            cn = cvx.matrix(cn_array)
-
-            p = cvx.matrix([0.0, 0.0, 0.0])
-            A = cvx.matrix(A)
-            cn = cvx.matrix(cn)
-
-            sol = cvx.solvers.qp(Q, p, A, cn)  # 调用优化函数solvers.qp求解
-            self.result = sol['x']
-            self._b = np.float64(self.result[0])
-            self._w1 =  np.float64(self.result[1])
-            self._w2 =  np.float64(self.result[2])
-            self.sketchMap()
-
-
-        def sketchMap(self):
-            correctListX1 = []
-            correctListX2 = []
-
-            errorListX1 = []
-            errorListX2 = []
-
-            for loop in range(self._Y.shape[0]):
-                if self._Y[loop] == 1:
-                    correctListX1.append(self._X[loop][0])
-                    correctListX2.append(self._X[loop][1])
-                else:
-                    errorListX1.append(self._X[loop][0])
-                    errorListX2.append(self._X[loop][1])
-
-            correctArrayX1 = np.array(correctListX1)
-            correctArrayX2 = np.array(correctListX2)
-            errorArrayX1   = np.array(errorListX1)
-            errorArrayX2    = np.array(errorListX2)
-
-            optimalY = []
-
-            for loop in range(self._X.shape[0]):
-                val = self._w1 * self._X[loop][0] + self._w2 * self._X[loop][1]+self._b
-                optimalY.append(val)
-
-            optimalYArray = np.array(optimalY)
-
-            fig, ax = plt.subplots(1, 1)
-            ax = fig.gca(projection='3d')
-            ax.scatter(correctArrayX1, correctArrayX2,c='g', marker="x")
-            ax.scatter(errorArrayX1, errorArrayX2,c='b', marker="+")
-
-            x1Array,x2Array = np.split(self._X,self._X.shape[1],axis = 1)
-            #ax.scatter(x1Array, x2Array, optimalYArray,c='orangered', marker=".")
-            x1Array = np.reshape(x1Array,(self._X.shape[0]))
-            x2Array = np.reshape(x2Array, (self._X.shape[0]))
-            print (type(x1Array),self._X.shape[0],x1Array.shape)
-            x1SplitResult = np.split(x1Array, [10, self._X.shape[0]],axis = 0)
-            x2SplitResult = np.split(x2Array, [10, self._X.shape[0]],axis = 0)
-            x1Array, x2Array = np.meshgrid(x1SplitResult[0], x2SplitResult[0])  # 将坐标向量变为坐标矩阵，列为x的长度，行为y的长度
-            print(x1Array.shape,x2Array.shape)
-            optimalYArray = self._w1 * x1Array + self._w2 * x2Array +self._b
-            ax.plot_surface(x1Array, x2Array, optimalYArray, rstride=1, cstride=1,color='coral', linewidth=0, antialiased=True,alpha=0.1)
-            ax.set_xlabel("x1-label", color='r')
-            ax.set_ylabel("x2-label", color='g')
-            ax.set_zlabel("Y-label", color='b')
-
-            ax.set_zlim3d(-50, 50)  # 设置z坐标轴
-
-            fig.suptitle('Linear SVM ')
-            plt.savefig("Linear_SVM.png")
-            plt.show()
-
-        def sketchTrainDataMap(self):
-            correctListX1 = []
-            correctListX2 = []
-
-            errorListX1 = []
-            errorListX2 = []
-
-            for loop in range(self._Y.shape[0]):
-                if self._Y[loop] == 1:
-                    correctListX1.append(self._X[loop][0])
-                    correctListX2.append(self._X[loop][1])
-                else:
-                    errorListX1.append(self._X[loop][0])
-                    errorListX2.append(self._X[loop][1])
-
-            correctArrayX1 = np.array(correctListX1)
-            correctArrayX2 = np.array(correctListX2)
-            errorArrayX1   = np.array(errorListX1)
-            errorArrayX2    = np.array(errorListX2)
-
-
-            fig, ax = plt.subplots(1, 1)
-            ax = fig.gca(projection='3d')
-            ax.scatter(correctArrayX1, correctArrayX2,c='g', marker="x")
-            ax.scatter(errorArrayX1, errorArrayX2,c='b', marker="+")
-            fig.suptitle('Linear SVM ')
-
-            plt.show()
-
-        def loadTrainData(self,fileLocation):
-            data = []
-            label = []
-            lineNum = 0
-            with open(fileLocation) as f:
-                line = f.readline()
-                while line:
-                    lineArray = line.split()
-                    for i in range(len(lineArray) - 1):
-                        data.append(float(lineArray[i]))
-                    label.append(int(lineArray[2]))
-                    line = f.readline()
-                    lineNum += 1
-            dataArray = np.array(data)
-            dataArray = dataArray.reshape(lineNum, 2)
-
-            labelArray = np.array(label)
-            self._X  = dataArray
-            self._Y = labelArray
-
-    linearSVMObj = LinearSVM()
-    linearSVMObj.__generateTrainData__()
-    #linearSVMObj.loadTrainData( 'G:\\林轩田教程\\MachineLearningFoundations\\homework5\\data\\question13_TRAIN.txt')
-    linearSVMObj.train()
-
-
-def DoDualSVM():
-
-    class DualSVM():
+    class SoftMarginSVM():
         def __init__(self):
             #self._b = np.zeros(1,dtype=np.float64)
             self._w = np.zeros(2)
@@ -224,12 +30,12 @@ def DoDualSVM():
                 Y[loop] = self.__sign__(W1 * X1[loop]*X1[loop] + W2 * X2[loop] * X2[loop] - B) # (x - a)^2 + (y -b)^2 - B 小于0，则在园内
 
 
-            '''
+            # soft-margin SVM 之前的SVM都无法解决噪声
             noiseArray = np.random.rand(Y.shape[0])
             for loop in range(Y.shape[0]):
                 if (noiseArray[loop]) > 0.9:
                     Y[loop] = -1 * Y[loop]
-            '''
+
 
             Xarray = np.stack((X1, X2), axis=-1)
 
@@ -241,6 +47,7 @@ def DoDualSVM():
 
 
         def __train__(self):
+            C = 20
             for loopn in range(self._Y.shape[0]):
                 for loopm in range(self._Y.shape[0]):
                     # (Z0,Z1,Z2) = Z = phy(x) = (1,x1,x2,x1*x1,x2*x2,x2*x2
@@ -276,10 +83,14 @@ def DoDualSVM():
             A_Matrix = cvx.matrix((np.array(self._Y)),(1,self._Y.shape[0]))
             b_Matrix = cvx.matrix(np.array([0.0]))
 
-            G = cvx.matrix(np.multiply(-1, np.identity(self._Y.shape[0])))
-            h = cvx.matrix(np.zeros((self._Y.shape[0],)))
+            #G = cvx.matrix(np.multiply(-1, np.identity(self._Y.shape[0])))
+            #h = cvx.matrix(np.zeros((self._Y.shape[0],)))
+            #sol = cvx.solvers.qp(P,q,G,h,A=A_Matrix, b=b_Matrix)  # 调用优化函数solvers.qp求解
 
-            sol = cvx.solvers.qp(P,q,G,h,A=A_Matrix, b=b_Matrix)  # 调用优化函数solvers.qp求解
+            G = cvx.matrix(np.multiply(1, np.identity(self._Y.shape[0])))
+            h = cvx.matrix(np.multiply(C,np.ones((self._Y.shape[0],))))
+
+            sol = cvx.solvers.qp(P,q,G,h)  # 调用优化函数solvers.qp求解
             self.result = sol['x']
             print (type(self.result),self.result.size)
             self._a = np.array(self.result)
@@ -315,7 +126,8 @@ def DoDualSVM():
                               self._X[loop][0] * self._X[loop][1]])
                 if self._a[loop] > 0:
                     print (x.shape)
-                    b = self._Y[loop] - np.dot(w,x)
+                    # b = self._Y[loop] - np.dot(w,x)
+                    b = C - self._a[loop]
                     break
             self._b = b
             self.__calcAccurateRatio__()
@@ -455,31 +267,9 @@ def DoDualSVM():
 
             plt.show()
 
-    dualSVMObj = DualSVM()
-    dualSVMObj.__generateTrainData__()
-    dualSVMObj.__train__()
-
-def SolveQPExample():
-    X = np.array([[0,0],[2,2],[2,0],[3,0]])
-    y = np.array([-1,-1,+1,+1])
-    Qd = np.array([[0.0,0.0,0.0,0.0],[0.0,8.0,-4.0,-6.0],[0.0,-4.0,4.0,6.0],[0.0,-6.0,6.0,9.0]])
-    print (Qd.shape)
-    Qd = cvx.matrix(Qd)
-    Ad = np.array([[-1.0,-1.0,1.0,1.0],[1.0,1.0,-1.0,-1.0],[1.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0]])
-    Ad = np.multiply(-1,Ad)
-    Ad = cvx.matrix(Ad)
-
-    q_array = np.multiply(-1, np.ones((4,)))
-    q = cvx.matrix(q_array)
-
-    h_array = np.zeros((4,))
-    h = cvx.matrix(h_array)
-    sol = cvx.solvers.qp(Qd, q, Ad, h)  # 调用优化函数solvers.qp求解
-    print (sol)
-
+    softMarginSVMObj = SoftMarginSVM()
+    softMarginSVMObj.__generateTrainData__()
+    softMarginSVMObj.__train__()
 
 if __name__ == "__main__":
-    #SolveQPExample()
-    #SolveQPWithCvxopt()
-    #DoLinearSVM()
-    DoDualSVM()
+    DoSoftMarginSVM()
